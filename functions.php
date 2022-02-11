63011356b9b86e06e58a4dd18fa8fe9801d051c2
sajjadcodes <sakraan01@gmail.com>
2022-02-11 05:23:15 +0500

    functions.php

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * MedPlus functions and definitions
+ *
+ * @package Medplus
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+// MedPlus's includes directory.
+$medplus_inc_dir = 'inc';
+
+// Array of files to include.
+$medplus_includes = array(
+	'/theme-settings.php',                  // Initialize theme default settings.
+	'/setup.php',                           // Theme setup and custom theme supports.
+	'/widgets.php',                         // Register widget area.
+	'/enqueue.php',                         // Enqueue scripts and styles.
+	'/template-tags.php',                   // Custom template tags for this theme.
+	'/pagination.php',                      // Custom pagination for this theme.
+	'/hooks.php',                           // Custom hooks.
+	'/extras.php',                          // Custom functions that act independently of the theme templates.
+	'/customizer.php',                      // Customizer additions.
+	'/custom-comments.php',                 // Custom Comments file.
+	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/medplus/medplus/issues/567.
+	'/editor.php',                          // Load Editor functions.
+	'/block-editor.php',                    // Load Block Editor functions.
+	'/deprecated.php',                      // Load deprecated functions.
+);
+
+// Load WooCommerce functions if WooCommerce is activated.
+if ( class_exists( 'WooCommerce' ) ) {
+	$medplus_includes[] = '/woocommerce.php';
+}
+
+// Load Jetpack compatibility file if Jetpack is activiated.
+if ( class_exists( 'Jetpack' ) ) {
+	$medplus_includes[] = '/jetpack.php';
+}
+
+// Include files.
+foreach ( $medplus_includes as $file ) {
+	require_once get_theme_file_path( $medplus_inc_dir . $file );
+}
